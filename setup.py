3d465158f711bc60ef81f8c8ff89b51beef6d9ae
Tyler Compton <xaviosx@gmail.com>
2017-05-29 04:09:19 -0700

    Add dependency management

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+        name='image_crawler',
+        version='1.0',
+        description='Recursively crawls webpages looking for images',
+        author='Alex Thiel and Tyler Compton',
+        packages=['PyQt5', 'selenium']
+        )
